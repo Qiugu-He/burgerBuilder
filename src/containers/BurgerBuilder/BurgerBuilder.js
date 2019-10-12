8bd5d5e7323623f99a34e6b1b74e2bc9458f047a
Qiugu He <kaochi545121151@gmail.com>
2019-10-12 00:47:11 -0500

    Rename bugged file

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Aux';
+import Aux from '../../ho/Aux';
 import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
     render () {
