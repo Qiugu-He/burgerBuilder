d3a03a48e48c772e0becc42607edf5a346ee6ea9
Qiugu He <kaochi545121151@gmail.com>
2019-10-12 00:57:04 -0500

    Removed bugged file

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Aux from '../../ho/Aux';
+import Aux from '../../hoc/ReactAux';
 import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
     render () {
