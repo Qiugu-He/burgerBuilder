69ba502e64d4fe6f5e8b642d07ae220d98fd02fb
Qiugu He <kaochi545121151@gmail.com>
2019-10-13 00:21:46 -0500

    Update OrderSummary.js

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -14,6 +14,7 @@ const orderSummary = (props) =>{
         }); 
     return (
         <Aux>
+            <h3>Your order</h3>
             <p>A delicious burger with the following ingredients: </p>
             <ul>
                 {ingredientsSummary}
