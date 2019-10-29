03682f178b417cc27d05ac2095b46fcf66c6586e
Qiugu He <kaochi545121151@gmail.com>
2019-10-29 00:12:49 -0500

    Adjustment sidebar for mobile model

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,7 +3,7 @@ import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = () =>(
     <ul className={classes.NavigationItems}>
-        <NavigationItem link = "/" active>Builder Builder</NavigationItem>
+        <NavigationItem link = "/" active>Burger Builder</NavigationItem>
         <NavigationItem link = "/">Check out</NavigationItem>
     </ul>
 );
