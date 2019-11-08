0117205261868ba2bb8fe7b378ad1bbd59c7b15a
Qiugu He <kaochi545121151@gmail.com>
2019-11-07 20:50:14 -0600

    Installed react-router-dom

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,16 +9,23 @@ const withErrorHandler = (WrappedComponet, axios )=>{
         state  = {
             error: null
         }
-        componentDidMount(){
-            axios.interceptors.request.use(req =>{
+        componentWillMount(){
+            this.reqInterceptor = axios.interceptors.request.use(req =>{
                 this.setState({error: null});
                 return req;
             });
 
-            axios.interceptors.response.use(res => res, error =>{
+            this.resInterceptor = axios.interceptors.response.use(res => res, error =>{
                 this.setState({error: error});
             });
         }
+        
+        componentWillUnmount(){
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
+
+        }
+
 
         errorConfirmedHandler = () =>{
             this.setState({error: null});
