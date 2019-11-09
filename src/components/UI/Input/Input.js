081fe17795fd6f63ddbfcfa1c6728ad4c0d56202
Qiugu He <kaochi545121151@gmail.com>
2019-11-08 22:56:43 -0600

    added a drop down buttom in input field

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -15,6 +15,18 @@ const input = (props) =>{
                 {...props.elementConfig} 
                 value = {props.value}/>
             break;
+        case('select'):
+            inputElement = (
+            <select 
+                className ={classes.inputElement} 
+                value = {props.value}>
+                {props.elementConfig.options.map(option=>(
+                    <option key ={option.value} value = {option.value}>
+                        {option.displayValue}
+                    </option>
+                ))}
+            </select>);
+            break;
         default:
             inputElement = <input 
                 className ={classes.inputElement} 
