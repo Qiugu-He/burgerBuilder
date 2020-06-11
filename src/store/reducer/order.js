636f58765713ad680b716142607514b199091e57
Qiugu He <kaochi545121151@gmail.com>
2020-06-11 17:53:33 -0500

    Fetched order from firebase via redux

--- src/store/reducer/order.js
+++ src/store/reducer/order.js
@@ -37,6 +37,24 @@ const reducer = (state = initialState, action) => {
                 loading: false,
                 orders: state.orders.concat(newOrder)
             };
+
+        case actionTypes.FETCH_ORDER_START:
+            return {
+                ...state,
+                loading: true
+            };
+
+        case actionTypes.FETCH_ORDER_SUCCESS:
+            return {
+                ...state,
+                orders: action.orders,
+                loading: false
+            };
+        case actionTypes.FETCH_ORDER_FAIL:
+            return {
+                ...state,
+                loading: false
+            };        
         default:
             return state;
     }
