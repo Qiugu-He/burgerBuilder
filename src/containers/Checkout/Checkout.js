b6b521382afcf3d19a6352ce53fa6e17f7d40b0c
Qiugu He <kaochi545121151@gmail.com>
2020-06-11 16:52:35 -0500

    updated reducer

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import ContactData from './ContactData/ContactData';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -33,18 +33,24 @@ class Checkout extends Component{
     }
 
     render (){
-        return (
-            <div>
-                <CheckoutSummary
-                    ingredients = {this.props.ings}
-                    checkoutCancelled = {this.checkoutCancelledHandler}
-                    checkoutContinued = {this.checkoutContinuedHandler}/>
-                <Route 
-                    path = {this.props.match.path+'/contact-data'} 
-                    //render= {(props)=>(<ContactData ingredients = {this.props.ings} price = {this.state.totalPrice} {...props}/>)}/>
-                    component={ContactData}/>
-            </div>
-        );
+        let summary = <Redirect to= "/" />
+        if(this.props.ings){
+            summary = (
+                <div>
+                    <CheckoutSummary
+                        ingredients = {this.props.ings}
+                        checkoutCancelled = {this.checkoutCancelledHandler}
+                        checkoutContinued = {this.checkoutContinuedHandler}/>
+                    
+                    <Route 
+                        path = {this.props.match.path+'/contact-data'} 
+                        //render= {(props)=>(<ContactData ingredients = {this.props.ings} price = {this.state.totalPrice} {...props}/>)}/>
+                        component={ContactData}/>
+                </div>
+            );
+        }
+
+        return summary;
     }
 }
 const mapStateToProps = state => {
