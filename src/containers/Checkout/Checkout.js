7a25ee964c7d0b5d17b27c9d73eb7065e7297119
Qiugu He <kaochi545121151@gmail.com>
2020-06-13 18:43:05 -0500

    Finished Authentication

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,7 +3,6 @@ import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import ContactData from './ContactData/ContactData';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
-import  * as actions from '../../store/actions/index';
 
 class Checkout extends Component{
 
