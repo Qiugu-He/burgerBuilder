5003bde045884dcf5fefdb17e9753fd1bab6a2b5
Qiugu He <kaochi545121151@gmail.com>
2020-06-13 18:17:27 -0500

    Enabled diaplaying specific user orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -26,7 +26,6 @@ export const purchaseBurgerStart = () => {
 export const purchaseBurger = (orderData, token) => {
     return dispatch => {
         dispatch(purchaseBurgerStart ());
-
         axios.post('/orders.json?auth='+token, orderData)
             .then(response => {
                dispatch(purchaseBurgerSuccess(response.data.name, orderData));
@@ -63,10 +62,11 @@ export const fetchOrderStart = () => {
     };
 };
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrderStart());
-        axios.get('/orders.json?auth=' + token )
+        const queryParams = '?auth='+token+'&orderBy="userId"&equalTo="'+userId+'"';
+        axios.get('/orders.json'+queryParams )
         .then(res =>{
             const fetchedOrders = []; 
             for(let key in res.data){
